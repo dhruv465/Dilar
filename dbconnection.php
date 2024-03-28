ad2964f54397431c95166021626185c7789b57c6
Dhruv <112197583+dhruv465@users.noreply.github.com>
2024-03-29 02:30:44 +0530

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,1 +1,15 @@
+<?php
 
+// Database connection parameters 156.67.222.1
+$host = 'yourhost';
+$username = 'dbusername';
+$password = 'dbpass';
+$database = 'dbname';
+
+// Create a connection
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Check the connection
+if (!$conn) {
+    exit('Connection failed: ' . mysqli_connect_error());
+}
